b57d4501f82089473dc729f181f247088191158c
Jason Gibson <65173587+JGibson37@users.noreply.github.com>
2021-06-10 15:04:58 -0400

    Update App.js
    Removed Default code. Added Header, input text box, and a state to save a word to search.

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -26,87 +19,52 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+export default function App() {
+  const work = true;
+  const [wordToSave, setWordToSave] = useState('');
+  const [savedWord, setSavedWord] = useState('Nothing');
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleSaveWordClick = () => {
+    setSavedWord(wordToSave);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <Text style={styles.headerStyle}>Beat Julia at Scrabble</Text>
+      <TextInput
+        style={styles.inputStyle}
+        autoCompleteType="off"
+        autoFocus={work}
+        selectionColor="red"
+        onChangeText={setWordToSave}
+      />
+      <Button
+        title="Search for Words"
+        type="submit"
+        onPress={handleSaveWordClick}
+      />
+      <Text style={styles.wordListStyle}>
+        Searching Words that match: {savedWord}
+      </Text>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
+  inputStyle: {
     marginTop: 32,
     paddingHorizontal: 24,
+    color: 'black',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  headerStyle: {
+    padding: 20,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  wordListStyle: {
+    paddingLeft: 20,
+    paddingTop: 20,
   },
 });
-
-export default App;
